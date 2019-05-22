8ebf7907cda75761036904a66edc8dfbd987e154
zhangjixu <1062904651@qq.com>
2019-05-22 16:23:56 +0800

    键盘事件

---
+++ lesson_03/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: "#app-vue",
+    data: {
+
+    },
+    methods: {
+        logName: function () {
+            console.log("你正在输入你的名字！");
+        },
+        logAge: function () {
+            console.log("你正在输入你的年龄！");
+        }
+    }
+});
