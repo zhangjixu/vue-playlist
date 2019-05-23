9043a4cc7ec9a83869595f09b7d9d5c4ece2fa66
zhangjixu <1062904651@qq.com>
2019-05-23 11:02:53 +0800

    v-for 循环

---
+++ lesson_08/app.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: "#app-vue",
+    data: {
+        characters: ["marry", "jack", "macic"],
+        users: [
+            {name: "herry", age: 20},
+            {name: "mack", age: 21},
+            {name: "lisi", age: 22}
+        ]
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+});
